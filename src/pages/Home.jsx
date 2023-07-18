5b0e72260c9df2f6acc050296ea84a200267febd
JoseTo21 <toro.jose.8e@gmail.com>
2023-07-18 14:01:20 -0400

    finished2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home__app">
       <img
         className="header__principal-home"
-        src="\public\68747470733a2f2f692e696d6775722e636f6d2f415975745a4f462e706e67.png"
+        src="68747470733a2f2f692e696d6775722e636f6d2f415975745a4f462e706e67.png"
       />
       <div>
         <h1>Hi Trainer!</h1>
